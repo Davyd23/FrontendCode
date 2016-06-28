dcacf0e0b1240c7c95d497439029be0d66553ffc
Davyd23 <daviddedu02@gmail.com>
2016-06-27 18:07:25 -0700

    day3

--- Day 1/frontdev/scripts/app.js
+++ Day 1/frontdev/scripts/app.js
@@ -123,3 +123,4 @@ function differentLastNames(){
     }
     return contor;
 }
+
--- FrontendCode/Day 2/scripts/MainController.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by user on 6/24/2016.
- */
-hrApp.controller("MainController",function($scope,$rootScope){
-    $scope.name="sdcdergregsc";
-});
